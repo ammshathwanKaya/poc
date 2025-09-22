40c5b561db23c1cffda38b63ebc9bd6ce8a17c47
amshathwan <ammshathwan@un4cus.com>
2025-09-22 21:36:16 +0530

    added new icon to sidebar

--- src/app/(app)/app/chat/components/chat/side-bar.tsx
+++ src/app/(app)/app/chat/components/chat/side-bar.tsx
@@ -7,7 +7,7 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
-import { Menu, X } from "lucide-react";
+import { ListVideo, Menu, MessageCircleMore, X } from "lucide-react";
 import { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
@@ -37,7 +37,7 @@ export const SideBar = () => {
       <DrawerContent className="w-screen max-w-screen bg-dark !border-0 text-light p-0">
         {/* 👆 makes drawer full width */}
         <DrawerHeader className="p-0">
-          <DrawerTitle className="flex items-center justify-end w-full text-light p-4 border-b-[1px] border-b-light/10">
+          <DrawerTitle className="flex items-center justify-end w-full text-light p-4 border-b-[1px] border-b-light/10 h-[64px]">
             <DrawerClose>
               <X size={20} />
             </DrawerClose>
@@ -48,21 +48,30 @@ export const SideBar = () => {
           <div className="p-4 flex flex-col gap-y-3 flex-grow">
             <button
               type="button"
-              className={cn("w-full text-left px-4 py-3 rounded-md", {
-                "bg-blue-500/10 border-blue-800 border-2 text-blue-300": isChat,
-              })}
+              className={cn(
+                "w-full text-left flex items-center gap-x-2 px-4 py-3 rounded-md",
+                {
+                  "bg-blue-500/10 border-blue-800 border-2 text-blue-300":
+                    isChat,
+                }
+              )}
               onClick={() => onclickRoute(true)}
             >
+              <MessageCircleMore size={20} />
               AI Coach
             </button>
             <button
               type="button"
-              className={cn("w-full text-left  px-4 py-3 rounded-md", {
-                "bg-blue-500/10 border-blue-800 border-2 text-blue-300":
-                  !isChat,
-              })}
+              className={cn(
+                "w-full text-left flex items-center gap-x-2  px-4 py-3 rounded-md",
+                {
+                  "bg-blue-500/10 border-blue-800 border-2 text-blue-300":
+                    !isChat,
+                }
+              )}
               onClick={() => onclickRoute(false)}
             >
+              <ListVideo size={20} />
               Video library
             </button>
           </div>
